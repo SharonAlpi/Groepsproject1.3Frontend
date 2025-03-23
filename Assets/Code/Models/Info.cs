847ee0e6a362378d001fced361dbf8d56dfb9a1b
Sharon Alpi <s1148724@student.hsleiden.nl>
2025-03-23 23:34:26 +0100

    route word nu bepaald op basis van route waarde in database (lokaal)

---
+++ Assets/Code/Models/Info.cs
@@ -0,0 +1,12 @@
+using System;
+
+[Serializable]
+public class Info
+{
+    public Guid UserId { get; set; }
+    public string Name { get; set; }
+    public string NameDocter { get; set; }
+    public bool Route { get; set; }
+    public DateTime BirthDay { get; set; }
+    public int AvatarId { get; set; }
+}
